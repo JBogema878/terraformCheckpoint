da1fe662fc799aac36d21ee838574a50b2af3ea1
James Bogema <james.bogema.lvyx@statefarm.com>
2022-12-09 13:36:44 -0600

    added security groups

--- main.tf
+++ main.tf
@@ -33,3 +33,38 @@ resource "aws_s3_object" "object" {
   # etag = "${md5(file("path/to/file"))}"
   etag = filemd5("./target/g-autos-0.0.1-SNAPSHOT.jar")
 }
+
+//security group
+
+resource "aws_security_group" "allow_tls" {
+  name        = "allow_tls"
+  description = "Allow TLS inbound traffic"
+  
+
+  ingress {
+    description      = "TLS from VPC"
+    from_port        = 8080
+    to_port          = 8080
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description      = "TLS from VPC"
+    from_port        = 22
+    to_port          = 22
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "allow_tls"
+  }
+}
