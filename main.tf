fa864e4af7b832c47d1881064613461a81669b48
James Bogema <james.bogema.lvyx@statefarm.com>
2022-12-09 11:55:28 -0600

    added jar file object into bucket

--- main.tf
+++ main.tf
@@ -11,6 +11,8 @@ provider "aws" {
   region = "us-east-1"
 }
 
+//create a bucket
+
 resource "aws_s3_bucket" "b" {
   bucket = "jim-tf-test-bucket"
 
@@ -19,7 +21,15 @@ resource "aws_s3_bucket" "b" {
   }
 }
 
-# resource "aws_s3_bucket_acl" "example" {
-#   bucket = aws_s3_bucket.b.id
-#   acl    = "private"
-# }
+//adding an object to that bucket, which in this case is my jar file for gautos spring boot app
+
+resource "aws_s3_object" "object" {
+  bucket = "jim-tf-test-bucket"
+  key    = "guatos_jarFile"
+  source = "./target/g-autos-0.0.1-SNAPSHOT.jar"
+
+  # The filemd5() function is available in Terraform 0.11.12 and later
+  # For Terraform 0.11.11 and earlier, use the md5() function and the file() function:
+  # etag = "${md5(file("path/to/file"))}"
+  etag = filemd5("./target/g-autos-0.0.1-SNAPSHOT.jar")
+}
